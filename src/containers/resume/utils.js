9a5e000083a1e5fd35b3790bb48f90627974c633
Gr3yH4tDud3 <srikrrishn@gmail.com>
2024-06-02 23:31:26 +0600

    Completed my project

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "Aviator ",
+        subTitle: "Bangalore India",
+        description:
+          " User Experience, Visual Design",
+      },
+      {
+        title: "Professional Gamer",
+        subTitle: "Jaipur, India",
+        description:
+          "Teamwork, team management, stratergy, Gaming skill",
+      },
+      {
+        title: "Rifle Shooter(National level)",
+        subTitle: "Bangalore & Jaipur & Guwahati, INDIA",
+        description:
+          "Sportsmanship, Physical fitness, Concentration",
+      },
+    ],
+    education: [
+      {
+        title: "Mahaveer International ",
+        subTitle: "School",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Mahaveer International",
+        subTitle: "School",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.Sc",
+        subTitle: "Scaler School of technology",
+        description:
+          "Bachelor in computer science",
+      },
+    ],
+  };
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,53 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "50",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "80",
+        },
+      ],
+    },
+    {
+      label: "Robotics",
+      data: [
+        {
+          skillName: "Drones",
+          percentage: "70",
+        },
+        {
+          skillName: "Arduino",
+          percentage: "65",
+        },
+        {
+          skillName: "Raspberry Pi",
+          percentage: "35",
+        },
+      ],
+    },
+    {
+      label: "Life",
+      data: [
+        {
+          skillName: "Stock market",
+          percentage: "70",
+        },
+        {
+          skillName: "Rifle Shooting",
+          percentage: "65",
+        },
+        {
+          skillName: "Travelling",
+          percentage: "85",
+        },
+      ],
+    },
+  ];
